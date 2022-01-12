942534fbfd92995ad4101767013bbc6a356163c6
Konner Hartman <konner.hartman@yahoo.com>
2022-01-11 19:24:00 -0600

    updated server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
