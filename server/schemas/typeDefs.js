8725ca40ac33641d6a2686cfc58cebca06e9d943
Konner Hartman <konner.hartman@yahoo.com>
2022-01-11 20:09:46 -0600

    added type user and book with data to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,21 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+    }
 
+    type Book {
+        authors: String!
+        description: String!
+        bookId: String!
+        image: String!
+        link: String!
+        title: String!
+    }
 `;
 
 module.exports = typeDefs;
