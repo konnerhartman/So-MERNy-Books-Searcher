ef1f85dca625dabfeb62e7005eac98add5d9d798
Konner Hartman <konner.hartman@yahoo.com>
2022-01-14 11:13:59 -0600

    added code to resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,19 @@
-const { User, Book } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+const { AuthError } = require('apollo-server-express')
 
 const resolvers = {
     Query: {
-        me: async (parents, args) => {
+        me: async (parents, args, context) => {
+            if (context.user) {
+                const userData = await User.findOneAndDelete({ _id: context.user_id }).select(
+                    "-__v -password"
+                );
 
+                return userData;
+            }
+
+            throw new AuthError("Not Logged In");
         }
     },
 
@@ -15,7 +24,34 @@ const resolvers = {
             return { token, user }
         },
 
-        login: async 
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthError("Not a valid username");
+            }
+
+            const correctPW = await user.isCorrectPassword(password);
+
+            if (!correctPW) {
+                throw new AuthError("Not a valid password");
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        }
+
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user_id },
+                    { $pull: { savedBooks: { bookId: args.bookId }}},
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthError("Please log in!");
+        }
     }
 };
 
