4d7e1eb859d83c011490501c7c04ce7963742486
Konner Hartman <konner.hartman@yahoo.com>
2022-01-14 13:31:28 -0600

    updated queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,27 +1,20 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
+export const GET_ME  = gql`
+  {
+    me {
       _id
       username
       email
-      book [book]
-    }
-  }
-`;
-
-
-export const QUERY_BOOK = gql`
-  query book {
-    book {
-      _id
-      authors
-      description
-      bookId
-      image
-      link
-      title
+      bookCount
+      savedBooks {
+        bookId
+        title
+        description
+        authors
+        link
+        image
+      }
     }
   }
 `;
